f05bfa564a85b248d29b4af112fb8048dfafc19e
Griffures <geotony550@gmail.com>
2018-12-06 21:14:51 +0100

    Found the truth

--- Sources/Interface/mainTests.c
+++ Sources/Interface/mainTests.c
@@ -7,32 +7,11 @@
 
 int main(){
 
-    char* trainerPath = "Saves/Bouee_x784_10L.bin";
+    char* trainerPath = "Saves/Clairiere_x784_3L.bin";
 
     T_Trainer* trainer = LoadTrainer(trainerPath);
 
-    //PrintNetworkTransitions(trainer->Network);
-
-    /*
-    double* values;
-    for (int i = 0; i < trainer->nbSets; ++i) {
-        values = trainer->SetsOfInputs[i];
-
-        for (int j = 0; j < 784; ++j) {
-            if (j % 28 == 0)
-                printf("\n|");
-            printf("%lf|", values[j]);
-        }
-
-        printf("\n\n");
-    }
-    */
-
-     ShowResults(trainer, 28);
-
-     PrintAllNetworkInfos(trainer->Network, 1);
-
-
+    PrintAllNetworkInfos(trainer->Network, 0);
 
     FreeTrainer(trainer);
 }
